31ef5c00594328dc9328520ac3991490680f10a2
deepakjava8 <deepakjava8@gmail.com>
2021-08-13 09:47:03 +0530

    updated method reference in java 8

---
+++ src/java8/dilips/FunctionMethodReferenceExample.java
@@ -0,0 +1,12 @@
+package java8.dilips;
+
+import java.util.function.Function;
+
+public class FunctionMethodReferenceExample {
+	static Function<String, String> toUpperCaseLambda=(s)->s.toUpperCase();
+	static Function<String, String> toUpperCaseMethodReference=String::toUpperCase;
+public static void main(String[] args) {
+	System.out.println(toUpperCaseLambda.apply("Java8"));
+	System.out.println(toUpperCaseMethodReference.apply("Java8"));
+}
+}
